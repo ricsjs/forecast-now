73ff44fd602f6786997a567e2ef70f665b016882
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-21 01:45:00 -0300

    buscando dados da API

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,18 @@ import api from './services/api';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [lat, setLat] = useState('');
-  const [lon, setLon] = useState('');
+  const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
-
-  //lat=39.31
-  //lon=-74.5
 
   const showInfo = async () => {
     try {
       const response = await api.get("/weather", {
         params: {
-          lat: lat,
-          lon: lon,
-          appid: "d54d9d02c25c79b822c4d38bbc3a1e47"
+          q: city,
+          units: "metric",
+          appid: "d54d9d02c25c79b822c4d38bbc3a1e47",
+          lang: "pt_br"
         }
       });
       setWeatherData(response.data);
@@ -28,29 +25,24 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    if (lat && lon) {
-      showInfo();
-    }
-  }, [lat, lon]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    showInfo();
+  };
 
   return (
     <div>
       <Header />
       <main className='main'>
         <h2>Previsão do tempo agora</h2>
-        <input
-          type="text"
-          value={lat}
-          onChange={(e) => setLat(e.target.value)}
-        />
-        <input
-          type='text'
-          value={lon}
-          onChange={(e) => setLon(e.target.value)}
-        />
-
-        {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button type='submit'>Pesquisar</button>
+        </form>
 
         {weatherData && (
           <div>
@@ -59,6 +51,8 @@ function App() {
             {/* Exibir mais informações do clima, se necessário */}
           </div>
         )}
+
+        {error && <p>{error}</p>}
       </main>
       <Footer />
     </div>
